56d3fe2b2a5ec77c1f2e1a06cf9d6f663f7614b9
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-11-01 14:36:48 -0700

    feat: implement repeatString logic

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) 
+{
+    let finalString = "";
 
+    if (num < 0)
+    {
+        return "ERROR";
+    }
+    for (i = 0; i < num; i++)
+    {
+        finalString += string;
+    }
+    return finalString;
 };
+
+// let string = "hello";
+// let num = 3;
+
+console.log(repeatString("hello", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
