f8e3ab8211e6bd54b1b8cd8cf1eb0421c48f6115
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-11-03 11:32:15 -0800

    feat: implement removeFromArray logic

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,4 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (arr, ...args) => arr.filter(arg => !args.includes(arg));
 
 // Do not edit below this line
 module.exports = removeFromArray;
