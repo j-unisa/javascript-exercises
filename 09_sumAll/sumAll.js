cb715f476a054b7eef47193077268fc57eb18bd5
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-11-03 12:02:29 -0800

    feat: implement sumAll logic

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Figure our which of the 2 arguments is smaller
+    // and use that as the for loop initializer
+    // then use the larger as the conditional
+    let small = 0;
+    let large = 0;
 
+    if (((a < 0) || (!Number.isInteger(a))) || ((b < 0) || (!Number.isInteger(b))))
+    {
+        return "ERROR";
+    }
+    else if (a < b)
+    {
+        small += a;
+        large += b;
+    }
+    else
+    {
+        small += b;
+        large += a;
+    }
+
+    let sum = 0;
+    for (i = small; i <= large; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
