77c9acd19e58ec975121d7579b20f386109726fa
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-11-23 09:32:21 -0800

    feat: implement all calculator functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,23 +1,20 @@
 const add = (x, y) => x + y;
 
-const subtract = function() {
-	
-};
+const subtract = (x, y) => x - y;
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce((total, current) => total + current, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((total, current) => total * current, 1);
 
-};
+const power = (base, exponent) => base ** exponent;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1;
+  for (let i = x; i > 0; i--)
+  {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
