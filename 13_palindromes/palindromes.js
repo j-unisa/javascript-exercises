d36cfa9d1e45e3488b33061052664961bcc656e9
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-11-24 10:32:07 -0800

    feat: implement palindromes logic

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Split into array characters
+    let arr = str.split("");
 
+    // Filter alphanumeric characters into a new array
+    let alphaNumeric = arr.filter((char) => /[a-zA-Z0-9]/.test(char));
+
+    // If joined array equals reversed-joined array
+    // Return true
+    if (alphaNumeric.join("").toLowerCase() == alphaNumeric.reverse().join("").toLowerCase())
+    {
+        return true;
+    }
+    // Else return false
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
