40a3509a51bb7966e755b0c4714523f0fc179701
j-unisa <104635026+j-unisa@users.noreply.github.com>
2025-11-25 16:06:00 -0800

    feat: implement fibonacci logic

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0)
+        {
+            return "OOPS";
+        }
+    let sequence = [0];
+    let number = 1;
 
+    for (let i = 1; i <= num; i++)
+    {        
+        if (!sequence.at(-2))
+        {
+            sequence.push(number);
+        }
+        else
+        {
+            sequence.push(sequence.at(-1) + sequence.at(-2));
+        }
+    }
+
+    return sequence.at(-1);
 };
 
 // Do not edit below this line
